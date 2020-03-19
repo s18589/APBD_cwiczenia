8559e323dd9cc4e9382f78d13eb9ec0c07f486be
matik <s18589@pjwstk.edu.pl>
2020-03-19 11:58:33 +0100

    cwiczenie 3 ukończone

---
+++ Cwiczenia3/Cwiczenia3/Controllers/StudentsController.cs
@@ -0,0 +1,60 @@
+﻿using Cwiczenia3.DAL;
+using Cwiczenia3.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cwiczenia3.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+        private readonly IDbService _dbService;
+        
+        public StudentsController(IDbService dbService)
+        {
+            _dbService = dbService;
+        }
+
+        [HttpGet]
+        public IActionResult GetStudents(string orderBy)
+        {
+            return Ok(_dbService.GetStudents());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetStudent(int id)
+        {
+            if (id == 1)
+            {
+                return Ok("Kowalski");
+            }else if (id == 2)
+            {
+                return Ok("Malewski");
+            }
+            return NotFound("Nie znaleziono studenta");
+        }
+
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+            return Ok(student);
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteStudent(string StudentId)
+        {
+            return Ok("Usuwanie ukończone");
+        }
+
+        [HttpPut]
+        public IActionResult PutStudent(string StudentId)
+        {
+            return Ok("Wstawianie ukonczone");
+        }
+    }
+}
