54f418af1ac5d51cf24e94f7459c54fd3f14f32a
Mateusz <s18589@pjwstk.edu.pl>
2020-04-09 11:13:14 +0200

    c06 zrobione

---
+++ Cwiczenia3/Cwiczenia3/Middlewares/LoggingMiddleware.cs
@@ -0,0 +1,57 @@
+﻿using Cwiczenia3.Services;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cwiczenia3.Middlewares
+{
+    public class LoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public LoggingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext, IStudentDbService service)
+        {
+            string sciezka = httpContext.Request.Path;
+            string querystring = httpContext.Request?.QueryString.ToString();
+            string metoda = httpContext.Request.Method.ToString();
+            string bodyString = "";
+
+            using(StreamReader reader = 
+                new StreamReader(httpContext.Request.Body, Encoding.UTF8, true, 1024, true))
+            {
+                bodyString = await reader.ReadToEndAsync();
+            }
+
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(sciezka);
+            stringBuilder.Append(querystring + " ");
+            stringBuilder.Append(metoda + " ");
+            stringBuilder.Append(bodyString + " ");
+            stringBuilder.Append(DateTime.Now);
+
+            StreamWriter streamWriter;
+
+            if (!File.Exists("logs.txt"))
+            {
+                streamWriter = new StreamWriter("logs.txt");
+            }
+            else
+            {
+                streamWriter = File.AppendText("logs.txt");
+            }
+            streamWriter.WriteLine(stringBuilder.ToString());
+            streamWriter.Close();
+
+            await _next(httpContext);
+        }
+    }
+}
--- Cwiczenia3/Cwiczenia3/Startup.cs
+++ Cwiczenia3/Cwiczenia3/Startup.cs
@@ -1,11 +1,14 @@
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using Cwiczenia3.DAL;
+using Cwiczenia3.Middlewares;
 using Cwiczenia3.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -40,6 +43,43 @@ namespace Cwiczenia3
             }
 
             app.UseHttpsRedirection();
+
+            app.UseMiddleware<LoggingMiddleware>();
+
+            app.Use(async (context, next) =>
+            {
+                if (!context.Request.Headers.ContainsKey("Index"))
+                {
+                    context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                    await context.Response.WriteAsync("Ni ma indexu");
+                    return;
+                }
+                string index = context.Request.Headers["Index"].ToString();
+
+                using(var connection = new SqlConnection("Data Source=db-mssql;Initial Catalog=s18589;Integrated Security=true"))
+                using(var command = new SqlCommand())
+                {
+                    connection.Open();
+                    command.Connection = connection;
+                    var transaction = connection.BeginTransaction();
+                    command.Transaction = transaction;
+
+                    command.Parameters.AddWithValue("index", index);
+                    command.CommandText = "select index from student where indexnumber = index";
+
+                    var dr = command.ExecuteReader();
+
+                    if (!dr.HasRows)
+                    {
+                        dr.Close();
+                        transaction.Rollback();
+                        context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                        await context.Response.WriteAsync("Ni ma indexu w bazie");
+                        return;
+                    }
+                }
+                await next();
+            });
 
             app.UseRouting();
 
